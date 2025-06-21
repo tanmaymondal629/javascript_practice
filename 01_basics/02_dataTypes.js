4e69284eb6597953f204826ee63da004f755185c
tanmaymondal629 <tanmaymondal629@gmail.com>
2025-06-21 23:03:33 +0530

    aaaa

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,7 @@
+"use strict" // treat all JS code as newer version
+// alert(3 + 3) // alert doesn't work on node.js it works on browser javascript engine
+
+console.table([typeof "Tanmay", typeof 123, typeof true, typeof null, typeof undefined])
+
+
+//interview question : different between null and undefined 
