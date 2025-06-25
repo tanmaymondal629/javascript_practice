6e6cca732e4d4a4aacd13ee9e120747d3fe10875
tanmaymondal629 <tanmaymondal629@gmail.com>
2025-06-25 04:21:30 +0000

    datatype convertion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -21,3 +21,49 @@ console.log("Boolean(1) value is: "+Boolean(1))//Number to Boolean = True
 console.log("Boolean(0) value is: "+Boolean(0))//Number to Boolean = False
 console.log("Boolean('') value is: "+Boolean(""))//Number to Boolean = False
 console.log("String(33) value is: "+String(33)+", And the datatype is "+ typeof String(33))//Number to Boolean = False
+
+/**************************************** Operstions ****************************************************/
+
+let value = 3
+let negvalue = - value
+console.log(negvalue);
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**2)// 2 to the power 2
+console.log(2/2)
+console.log(2%2);
+
+let str1 = "hello "; let str2 = "world"
+str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+ true)
+// console.log(true +) // syntax error
+console.log(+ "")
+
+// pre-increment & post-increment
+
+let gamecounter = 100
+// console.log(gamecounter++);
+console.log(++gamecounter);
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
+/* MDN LINK: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment */
